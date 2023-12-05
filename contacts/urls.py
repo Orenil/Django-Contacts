fedcefbf28bb9ffbd2410bbcc96c0b06853ba103
Orenil <oreoluwaadesina1999@gmail.com>
2023-12-06 00:45:01 +0100

    inital commit

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('profile/', views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name= 'logout'),
     path('contact-list/', views.contact_list, name='contact-list'),
     path('upload-csv/', views.upload_csv, name='upload_csv')
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Contact
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
+from .forms import UserRegisterForm
 import csv
 
 def login(request):
@@ -20,13 +22,19 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your Account has been created! Please login')            
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
 
 def home(request):
     home = Contact.objects.all()
