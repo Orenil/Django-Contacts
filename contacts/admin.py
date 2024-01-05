a87aae8fd5c1d366ba19d181f5865961f32f8f66
Orenil <oreoluwaadesina1999@gmail.com>
2024-01-05 21:30:07 +0100

    updated admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -9,9 +9,6 @@ class ContactAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'phone', 'title', 'company', 'type', 'location', 'level')
     search_fields = ('first_name', 'last_name', 'email', 'company', 'title')
 
-
-admin.site.register(Contact, ContactAdmin)
-
 class Campaign_EmailsAdmin(admin.ModelAdmin):
     list_display = ('user', 'email','first_name', 'last_name', 'company', 'type', 'location', 'title', 'campaign_name')
 
