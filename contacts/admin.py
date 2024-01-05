665b05879d17d6253cadb8ea83ad8cd1c5b484a2
Orenil <oreoluwaadesina1999@gmail.com>
2024-01-05 21:28:26 +0100

    updated admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,32 +1,16 @@
 import csv
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from .models import Contact
 from .models import Campaign_Emails, Campaign, Profile
-class ContactAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'phone', 'title', 'company', 'type', 'location','level')
+
+@admin.register(Contact)  # Using @admin.register decorator instead of admin.site.register
+class ContactAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'phone', 'title', 'company', 'type', 'location', 'level')
     search_fields = ('first_name', 'last_name', 'email', 'company', 'title')
-    
-    # Define a custom admin action for importing CSV
-    def import_csv(self, request, queryset):
-        # This action assumes the CSV file has the same structure as the model fields
-        # You may need to modify this logic based on your specific CSV format and model
-        for obj in queryset:
-            # Assuming 'csv_file' is the field name for the CSV file
-            if obj.csv_file:
-                csv_file = obj.csv_file.read().decode('utf-8').splitlines()
-                csv_reader = csv.DictReader(csv_file)
-                for row in csv_reader:
-                    # Create objects from CSV data
-                    Contact.objects.create(**row)
-        self.message_user(request, "CSV file imported successfully.")
-    
-    import_csv.short_description = "Import CSV"  # Display name for the action in the admin interface
-    # Register the custom action in the admin interface
-    actions = ['import_csv']
 
 
 admin.site.register(Contact, ContactAdmin)
-
 
 class Campaign_EmailsAdmin(admin.ModelAdmin):
     list_display = ('user', 'email','first_name', 'last_name', 'company', 'type', 'location', 'title', 'campaign_name')
