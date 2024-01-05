03d946bc07800c3cc9fd0c7f06dffce068af652b
Orenil <oreoluwaadesina1999@gmail.com>
2024-01-06 00:20:01 +0100

    updated changes

--- contacts/utils.py
+++ contacts/utils.py
@@ -1,10 +1,12 @@
 class ImportUtils:
-    def __init__(self, column_headers):
+    def __init__(self,column_headers):
         self.column_headers = column_headers
 
-    def get_column(self, header):
-        try:
-            return self.column_headers.index(header)
-        except ValueError:
-            # Handle the case when the header is not found in column_headers
+    def get_column(self,header):
+        return self.column_headers.index(header)
+    
+    def validate_data(self,data):
+        if not data or data == "":
             return None
+        else:
+            return data.strip()
