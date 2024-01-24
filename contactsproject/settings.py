53bea12672377c8a00931e4c134e1de8136ebc2f
Orenil <oreoluwaadesina1999@gmail.com>
2024-01-24 17:58:47 +0100

    updated changes

--- contactsproject/settings.py
+++ contactsproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['networkingapp-09bd564b3d1e.herokuapp.com']
+ALLOWED_HOSTS = ['networkingapp-09bd564b3d1e.herokuapp.com', 'www.followupnetworking.com', 'followupnetworking.com']
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # Or another session engine of your choice
 
